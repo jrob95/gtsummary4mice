ad507dfe635b5d246df8d0899ceb914ad3f63644
jrob95 <jack.m.roberts@gmail.com>
2024-12-19 18:07:53 +1000

    fix tests

--- tests/testthat/test-tbl_uvregression_mids.R
+++ tests/testthat/test-tbl_uvregression_mids.R
@@ -14,24 +14,24 @@ test_that("tbl_uvregression.mids works with mids objects", {
   data$predictor2[sample(1:100, 20)] <- NA  # Introduce missing values
 
   # Create mids object using mice
-  imputed_data <- mice(data, m = 5, maxit = 5, seed = 123, printFlag = FALSE)
+  imputed_data <- mice::mice(data, m = 5, maxit = 5, seed = 123, printFlag = FALSE)
 
   # Run tbl_uvregression on mids object
   tbl <- tbl_uvregression(
     imputed_data,
     method = glm,
-    y = outcome ~ .,
+    y = outcome,
     exponentiate = TRUE
   )
 
   # Tests
-  expect_s3_class(tbl, "tbl_uvregression")         # Check correct class
-  expect_true("tbl_summary" %in% class(tbl))       # Check tbl_summary is part of the class
+  expect_true("tbl_uvregression" %in% class(tbl))       # Check tbl_uvregression is part of the class
+  # expect_s3_class(tbl, "tbl_uvregression")         # Check correct class
+  expect_true("gtsummary" %in% class(tbl))       # Check tbl_summary is part of the class
   expect_named(tbl$table_body,                     # Ensure table has expected columns
-               c("variable", "estimate", "ci", "p.value"))
-
-  # Test pool_and_tidy_mice integration
-  expect_true(any(grepl("pooled", tolower(names(tbl)))))  # Confirm pooled results
+               c('tbl_id1', 'variable', 'var_label', 'var_type', 'reference_row', 'row_type', 'header_row', 'coefficients_type', 'coefficients_label', 'label', 'term', 'var_class', 'var_nlevels', 'contrasts', 'contrasts_type', 'estimate', 'std.error', 'statistic', 'b', 'df', 'dfcom', 'fmi', 'lambda', 'm', 'riv', 'ubar', 'n', 'ci', 'conf.low', 'conf.high', 'p.value'),
+               ignore.order = FALSE,
+               ignore.case = FALSE)
 
   # Check that no error occurs when printing
   expect_no_error(as_gt(tbl))
@@ -41,10 +41,12 @@ test_that("tbl_uvregression.mids works with mids objects", {
   tbl_single <- tbl_uvregression(
     imputed_data,
     method = glm,
-    y = outcome ~ predictor1,
+    y = outcome,
+    include = predictor1,
     exponentiate = TRUE
   )
-  expect_s3_class(tbl_single, "tbl_uvregression")
+  expect_true("tbl_uvregression" %in% class(tbl_single))       # Check tbl_uvregression is part of the class
+  # expect_s3_class(tbl_single, "tbl_uvregression")
 
   # # Test invalid inputs
   # expect_error(tbl_uvregression(data, method = glm, y = outcome ~ .),
